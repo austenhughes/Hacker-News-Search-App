331798c42700c9c72c08ca543e9acb12bdb938c2
Austn Hughes <austen.hughes.91@gmail.com>
2021-09-28 12:42:02 -0600

    react set up

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+// import Home from "./pages/Home/index";
+// import Portfolio from "./pages/Portfolio/index";
+
+import './App.css';
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Switch>
+
+        {/* <Route  exact path={["/", "/search"]}>
+            <Search />
+          </Route> */}
+
+        {/* <Route path={["/history"]}>
+            <History />
+          </Route> */}
+
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
