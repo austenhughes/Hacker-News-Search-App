824d0aa4ea5d5af91d232a3deec5ce03c11df4e5
Austn Hughes <austen.hughes.91@gmail.com>
2021-09-28 18:07:38 -0600

    search bar works API call is writing to console

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState} from "react";
+import { Button } from '@material-ui/core';
 
-function Searchbar() {
-return (
-<div className="searchbar">
-    <input type="text"></input>
-    <button>search</button>
-</div>
+function Searchbar(props) {
+
+    const [newSearch, setNewSearch] = useState({})
+
+    function handleInputChange(event) {
+        const { value } = event.target;
+        setNewSearch({...newSearch, value})
+        };
+
+    function handleFormSubmitNewSearch(event) {
+        event.preventDefault();
+        // console.log(newSearch);
+        let forCall = newSearch.value;
+        console.log(forCall)
+
+        fetch(
+            "http://hn.algolia.com/api/v1/search?query="+forCall+"&tags=story"
+        )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let information = data
+            console.log(information)
+        });
+
+          };
+
+    return (
+        <div className="searchbar">
+        
+        <input
+          onChange={handleInputChange}
+          value={props.value}
+          name="search"
+          type="text"
+          className=""
+          placeholder="search"
+          id="search"
+        />
+        
+        <Button 
+            onClick={handleFormSubmitNewSearch} 
+            className="btn" type="button" value="Search">Search
+        </Button>
+       
+        </div>
 );
+
 }
 
 export default Searchbar; 
--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Display from "../../components/Display";
 import Nav from "../../components/Nav/index";
 import Record from "../../components/Record/index";
 import Footer from "../../components/Footer/index";
